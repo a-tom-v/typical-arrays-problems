6292717165b75ca02c3cca9caec610f15f8a7a77
Artsem Tamasheuski <atom671@gmail.com>
2020-09-30 23:42:50 +0300

    fix: fixed code in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
+  let minResult = Math.min.apply(null, array);
+  if(!array || array.length === 0){
+    return 0;
+  } else {
+    return minResult;
+  }
+};
 exports.max = function max (array) {
+  let maxResult = Math.max.apply(null, array);
+  if(!array || array.length === 0){
   return 0;
+} else {
+  return maxResult;
 }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(!array || array.length === 0){
+    return 0;
+  } else {
+    let sumResult = array.reduce(function(sum, current) {
+      return sum + current;
+    }, 0);
+    let avgResult = sumResult / array.length;
+    return avgResult;
+  }
+};
